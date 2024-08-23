44c5f1115bec9dd9ea774165428adad846a962a0
EarlyBirdAcademy <iBinJiang@outlook.com>
2024-08-23 21:45:15 +0800

    before flask

---
+++ hello.py
@@ -0,0 +1,25 @@
+def main():
+    mypoint =Point(3.0,5.0)
+    print(mypoint.heng)
+    print(f"the y zuobiao is {mypoint.zong}")
+   
+
+
+
+
+
+
+class Point:
+    def __init__(self,x,y):
+        self.heng = x
+        self.zong= y
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
